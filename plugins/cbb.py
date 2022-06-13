566284a14b58ef39bd1a038663eac0fc757a793d
Mon <91760182+thismn@users.noreply.github.com>
2022-06-13 15:27:26 +0700

    lali

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : This Person\n○ Language : <code>Python3</code>\n○ Channel : [Channel](https://t.me/yaagtau)\n○ Source Code : <a href='https://github.com'>Click here</a></b>",
+            text = f"<b>○ Creator : This Person\n○ Language : <code>Python3</code>\n○ Channel : <a href='https://t.me/yaagtau'>Channel</a>\n○ Source Code : <a href='https://github.com'>Click here</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
